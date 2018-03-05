fe360b71aa842d80c53ba2b963ae3f11932d6204
Barry Tarlton <btarlton@gmail.com>
2018-03-04 23:29:10 -0500

    only show one button in production mode

--- src/pages/tree-control/tree-control.ts
+++ src/pages/tree-control/tree-control.ts
@@ -17,8 +17,7 @@ export class TreeControlPage {
   sceneButtonInfo: Map<string, boolean>;
   productionMode: boolean = true;
   static sceneNameList = new Array("step1", "step2", "step3", "step4", "step5", "step6", "step7", "step8", "step9", "step10", "step11", "step12", "step13", "step14", "step15", "step16", "step17", "step18", "step19", "step20", "step21");
-
-
+  
   constructor(public httpClientParm: HttpClient, private storage: Storage, private toast: Toast) {
     console.log("in TreeControl Page constructor");
     this.httpClient = httpClientParm;
